a65bb58908d3b7968e0653c8285d67a85b7acf4e
Nelson Segarra <junicus@outlook.com>
2022-08-18 17:57:29 -0400

    navbar and some sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./layout/Layout";
+import { LandingPage, RSVPPage, SchedulePage, WeddingPartyPage } from "./pages";
 
 function App() {
   return (
-    <div className="flex flex-col items-center gap-10">
-      <div className="font-livishly text-6xl">Adriana + Jose</div>
-      <p className="font-noto font-semibold text-xl">
-        4.<span className="text-2xl">23</span>.17
-      </p>
-      <p className="max-w-md text-center">
-        On the anniversary of when we first met, we went back to our old
-        stomping grounds to do karaoke with friends. When "I'd Do Anything for
-        Love" came on, we ran up to the front. When the music suddenly died
-        down, I looked over and he was down on one knee. I screamed, shouted
-        "YES," and we were engaged.
-      </p>
-      <p className="max-w-md text-center">
-        We're so excited to celebrate this next chapter in our lives with you.
-      </p>
-      <p className="max-w-md text-center">See you in San Fran!</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="schedule" element={<SchedulePage />} />
+        <Route path="wedding_party" element={<WeddingPartyPage />} />
+        <Route path="rsvp" element={<RSVPPage />} />
+      </Route>
+    </Routes>
   );
 }
 
